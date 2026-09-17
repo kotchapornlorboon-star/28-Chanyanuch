cab87167c10ec0e40e649a14913edc70f367731f
kotchapornlorboon-star <kotchaporn.lorboon@gmail.com>
2026-09-17 10:08:02 +0700

    Update Examination_2.py

--- Examination_2.py
+++ Examination_2.py
@@ -6,4 +6,9 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-temp = float(input())
+temp = float(input("อุณหภูมิร่างกาย"))
+
+if : อุณหภูมิร่างกาย >= 37.5
+    print("Fever")
+else :
+    print("normal")
